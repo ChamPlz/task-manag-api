ec654ae1e7150f14be4d0b5fedc22b8b7045ce58
ChamPlz <carlos_junior41@hotmail.com>
2026-01-03 20:14:34 -0300

    add user management module with controller, service, and DTO; integrate bcrypt for password hashing

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { UserDto } from './users.dto';
+import { v7 as uuidv7 } from 'uuid';
+import { hashSync as bcryptHashSync } from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+    private readonly users: UserDto[]=[
+        { id: uuidv7(), username: 'admin', password: 'admin' }
+    ]
+
+    create(newUser: UserDto) {
+        newUser.id = uuidv7();
+        newUser.password = bcryptHashSync(newUser.password, 10);
+        this.users.push(newUser);
+    }
+}
