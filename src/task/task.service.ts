1a90a89792cf304cd362970228a78584b63215b2
ChamPlz <carlos_junior41@hotmail.com>
2026-01-03 18:31:58 -0300

    implement task management module with controller, service, and DTO; remove unused files

---
+++ src/task/task.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { TaskDto } from './task.dto';
+
+@Injectable()
+export class TaskService {
+
+    private tasks: TaskDto[] = [];
+
+    createTask(task: TaskDto) {
+        this.tasks.push(task);
+        console.log(this.tasks);
+        return task;
+    }
+}
